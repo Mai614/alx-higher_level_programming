40a7ba7fa56cc60c106333a0066f31c7b06a37c0
Mai614 <maymohamed614@gmail.com>
2023-06-06 16:47:51 -0700

    --

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def uppercase(string):
+    for char in string:
+        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print("{}".format(uppercase_char), end="")
+    print()
