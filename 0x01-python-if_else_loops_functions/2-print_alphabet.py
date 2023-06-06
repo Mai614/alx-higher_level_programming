60ba9f52379f00567b293c51319c2cc63f3a6e1d
Mai614 <maymohamed614@gmail.com>
2023-06-06 15:48:03 -0700

    -

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+#for i in range(97, 123):
+   # print("{}".format(chr(i)), end="")
 
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
