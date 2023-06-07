ae9e4060b7bb0599ddd34083d5c5a8bbf7d90866
Mai614 <maymohamed614@gmail.com>
2023-06-06 18:06:22 -0700

    ---

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 i = 0
-for l in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(l - i)), end="")
+for letter in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(letter - i)), end="")
     i = 32 if i == 0 else 0
