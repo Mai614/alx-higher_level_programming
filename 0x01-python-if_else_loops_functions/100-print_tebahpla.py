c9a805d56fb8cc42760e2b45a6f1f33d79aa1194
Mai614 <maymohamed614@gmail.com>
2023-06-06 17:20:24 -0700

    ---

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a') - 1, -1):
+for i in range(90, 64, -1):
+    print(chr(i + 32), end="")
     print(chr(i), end="")
-    print(chr(i - (i % 32)), end="")
