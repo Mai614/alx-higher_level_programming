83e906b5fa0990d86d8f1faa6ab476966a6cc015
Mai614 <maymohamed614@gmail.com>
2023-07-03 03:00:13 -0700

    6-rect

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,29 +1,43 @@
+#!/usr/bin/python3
+
 class Rectangle:
+    """Rectangle class"""
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
+        """Initialize width and height attributes"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        """Return a string representation of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        size = "#" * self.__width
+        rect = []
+        for index in range(self.__height):
+            rect.append(size)
+        return "\n".join(rect)
+
+    def __repr__(self):
+        """Return a string representation that can be used to recreate the object"""
+        return "{:s}({:d}, {:d})".format((type(self).__name__), self.__width, self.__height)
+
+    def __del__(self):
+        """Destructor method that is called when the object is about to be destroyed"""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
 
     @property
     def width(self):
-        """
-        Retrieve the width of the rectangle.
-
-        """
+        """Getter method for width attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Args:
-            value (int): The width value to set.
-
-        Raises:
-            TypeError: If the width is not an integer.
-            ValueError: If the width is less than 0.
-        """
-        if not isinstance(value, int):
+        """Setter method for width attribute"""
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,67 +45,24 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Retrieve the height of the rectangle.
-
-        """
+        """Getter method for height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Set the height of the rectangle.
-
-        Args:
-            value (int): The height value to set.
-
-        Raises:
-            TypeError: If the height is not an integer.
-            ValueError: If the height is less than 0.
-        """
-        if not isinstance(value, int):
+        """Setter method for height attribute"""
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """
-        Calculate and return the area of the rectangle.
-
-        """
-        return self.width * self.height
+        """Calculate and return the area of the rectangle"""
+        return self.__height * self.__width
 
     def perimeter(self):
-        """
-        Calculate and return the perimeter of the rectangle.
-
-        """
-        if self.width == 0 or self.height == 0:
-            return 0
-        return 2 * (self.width + self.height)
-
-    def __str__(self):
-        """
-        Returns:
-            str: The rectangle representation with '#' characters.
-        """
-        if self.width == 0 or self.height == 0:
-            return ""
-        return '\n'.join(['#' * self.width] * self.height)
-
-    def __repr__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-            str: The string representation of the rectangle.
-        """
-        return f"Rectangle({self.width}, {self.height})"
-
-    def __del__(self):
-        """
-        Destructor method for the Rectangle class.
-
-        """
-        print("Bye rectangle...")
+        """Calculate and return the perimeter of the rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return None
+        return (self.__height * 2) + (self.__width * 2)
