9baca498aa57a1fc8dcd926bd3823b1f227388f0
Mai614 <maymohamed614@gmail.com>
2023-07-03 02:26:27 -0700

    pass

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,7 @@
+#!/usr/bin/python3
+"""defines a Rectangle"""
+
+
 class Rectangle:
-    """
-    This is a class that defines a rectangle.
-    """
-    def __init__(self):
-   	"""
-        Constructor method for the Rectangle class.
-        """
-        pass
+    """Rectangle class"""
+    pass
