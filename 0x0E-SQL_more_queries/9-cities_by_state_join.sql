baa8d6bc7eacc5367526341f5445c8b515d6510e
Mai614 <maymohamed614@gmail.com>
2023-08-16 14:17:34 -0700

    Genre ID by show

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
--- Retrieve the 'id' and 'name' columns from the 'cities' table (aliased as 'a'),
-
+-- Retrieve the 'id' and 'name' columns from the 'cities' table (aliased as 'a')
 SELECT a.id AS id, a.name AS city_name, b.name AS state_name
 FROM cities a
 INNER JOIN states b
