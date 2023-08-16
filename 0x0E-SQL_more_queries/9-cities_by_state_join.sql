a6c03160f4b4724e52829c0d89f7a4f0b89c6b68
Mai614 <maymohamed614@gmail.com>
2023-08-16 14:09:51 -0700

    8-9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Retrieve the 'id' and 'name' columns from the 'cities' table (aliased as 'a'),
+
+SELECT a.id AS id, a.name AS city_name, b.name AS state_name
+FROM cities a
+INNER JOIN states b
+ON a.state_id = b.id
+ORDER BY a.id ASC;
