4041b7a6f53c9f759cedc626ee2d36cd8fc9ee39
Mai614 <maymohamed614@gmail.com>
2023-09-11 17:34:19 -0700

    5-6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints 3 lines using for loop
+const sen = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < sen.length; i++) {
+  console.log(sen[i]);
+}
