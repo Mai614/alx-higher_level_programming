e86117ce82d6d9b2edfcf5191d79027184ed9080
Mai614 <maymohamed614@gmail.com>
2023-09-16 08:54:38 -0700

    edit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa """
+""" a script that takes in arguments and displays all values\
+     in the states table of hbtn_0e_0_usa """
 
 import MySQLdb
 import sys
