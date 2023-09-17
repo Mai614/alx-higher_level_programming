7abd1df53439964cccf31d602f9ddc1e1dfb6bd4
Mai614 <maymohamed614@gmail.com>
2023-09-17 15:03:01 -0700

    fix model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata
+Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """ Class with id and name of state"""
