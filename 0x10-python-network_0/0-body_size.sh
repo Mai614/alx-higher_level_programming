9187a1324a1af0b4f56a5d46bebaff1265adcf9b
Mai614 <maymohamed614@gmail.com>
2023-09-29 01:10:54 +0000

    edit 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL
 # and displays the size of the body of the response
-
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
