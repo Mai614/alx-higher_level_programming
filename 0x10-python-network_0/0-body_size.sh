67f51aeeb77770e4593ef8aef86749d10d53374d
Mai614 <maymohamed614@gmail.com>
2023-09-29 01:29:48 +0000

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL
 #and displays the size of the body of the response
-response_size=$(curl -s "$1" | wc -c)
-echo "$response_size"
+curl -so /dev/null -w '%{size_download}\n' "$1"
