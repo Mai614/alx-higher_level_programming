4b049fd56e97ca9cf100892f3fdfd2974a93dd0b
Mai614 <maymohamed614@gmail.com>
2023-10-24 15:51:55 +0000

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
+//script that prints the title of a Star Wars movie where the episode number matches a given integer
 
 const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  } else {
-    const movieData = JSON.parse(body);
-
-    if (movieData.detail === 'Not found') {
-      console.error(`Movie with ID ${movieId} not found.`);
-    } else {
-      console.log(`Title: ${movieData.title}`);
-    }
-  }
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body);
+  console.log(body.title);
 });
-
